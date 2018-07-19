507d41164ab18222b36bb5af62fcaade76578972
ripple_k <zhangkun@feizaotai.com>
2018-07-19 17:49:06 +0800

    Initial Commit

---
+++ ABTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ABTest
+//
+//  Created by ripple_k on 2018/7/19.
+//  Copyright © 2018 SoapVideo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
